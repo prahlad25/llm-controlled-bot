eac59fed5b7cdef7a55846da43bc56b05cce8d66
Prahlad Vivek <prahladvivek2001@gmail.com>
2026-01-28 17:05:07 -0500

    Added control and setting up environment

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,39 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    # Include the robot_state_publisher launch file. Force sim time to be enabled
+
+    package_name='llm-controlled-bot'
+
+    rsp = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+            )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # include gazebo launch file
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')])
+    )
+
+    # running robot spawn launch node
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'], 
+                                   output='screen')
+    
+    return LaunchDescription([
+        rsp, 
+        gazebo, 
+        spawn_entity
+    ])
